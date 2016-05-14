918d175ab7db4fe70c356286d70ae4881b67cb14
Dimiter Petrov <crackofdusk@gmail.com>
2016-05-14 20:54:01 +0200

    Add ball

--- Main.elm
+++ Main.elm
@@ -1,8 +1,8 @@
 import Html.App as App
 import Html exposing (Html, div)
 import Html.Attributes exposing (style)
-import Svg exposing (Svg, svg, rect)
-import Svg.Attributes exposing (width, height, x, y)
+import Svg exposing (Svg, svg, rect, circle)
+import Svg.Attributes exposing (width, height, x, y, cx, cy, r)
 import Mouse exposing (Position)
 import String
 
@@ -21,61 +21,10 @@ main =
 
 
 type alias Model =
-    { padX : Int }
-
-
-init : (Model, Cmd Msg)
-init =
-    ({padX = 0}, Cmd.none)
-
-
-
--- UPDATE
-
-
-type Msg
-    = PadMove Position
-
-
-update : Msg -> Model -> (Model, Cmd Msg)
-update message model =
-    case message of
-        PadMove position ->
-            ({ model | padX = position.x }
-            , Cmd.none)
-
-
-
--- VIEW
-
-
-view : Model -> Html Msg
-view model =
-    let
-        bound = gameAttributes.width - padAttributes.width
-        padX = clamp 0 bound model.padX
-    in
-        div
-            [ style
-                [ "width" => px gameAttributes.width
-                , "height" => px gameAttributes.height
-                , "background-color" => "#efe"
-                ]
-            ]
-            [ svg
-                [ width (toString gameAttributes.width)
-                , height (toString gameAttributes.height)
-                ]
-                [ pad padX ]
-            ]
-
-
-(=>) = (,)
-
-
-px : Int -> String
-px pixels =
-    toString pixels ++ "px"
+    { padX : Int
+    , ballX : Int
+    , ballY : Int
+    }
 
 
 gameAttributes =
@@ -89,6 +38,75 @@ padAttributes =
     , height = 20
     , top = 500
     }
+
+
+ballAttributes =
+    { radius = 10 }
+
+
+init : (Model, Cmd Msg)
+init =
+    let
+        midX = round (toFloat gameAttributes.width / 2)
+        halfPadWdith = round (toFloat padAttributes.width / 2)
+        model =
+            { padX = midX - halfPadWdith
+            , ballX = midX
+            , ballY = padAttributes.top - ballAttributes.radius
+            }
+    in
+        (model, Cmd.none)
+
+
+-- UPDATE
+
+
+type Msg
+    = PadMove Position
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update message model =
+    case message of
+        PadMove position ->
+            let
+                x = position.x - round (toFloat padAttributes.width / 2)
+                bound = gameAttributes.width - padAttributes.width
+                boundedX = clamp 0 bound x
+            in
+                ({ model | padX = boundedX }
+                , Cmd.none)
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div
+        [ style
+            [ "width" => px gameAttributes.width
+            , "height" => px gameAttributes.height
+            , "background-color" => "#efe"
+            ]
+        ]
+        [ svg
+            [ width (toString gameAttributes.width)
+            , height (toString gameAttributes.height)
+            ]
+            [ pad model.padX
+            , ball model.ballX model.ballY
+            ]
+        ]
+
+
+(=>) = (,)
+
+
+px : Int -> String
+px pixels =
+    toString pixels ++ "px"
 
 
 block : Int -> Int -> Int -> Int -> Svg Msg
@@ -105,6 +123,16 @@ block x' y' width' height' =
 pad : Int -> Svg Msg
 pad x =
     block x padAttributes.top padAttributes.width padAttributes.height
+
+
+ball : Int -> Int -> Svg Msg
+ball x y =
+    circle
+        [ cx (toString x)
+        , cy (toString y)
+        , r (toString ballAttributes.radius)
+        ]
+        []
 
 
 
